7fe84ddd6247509d47aa82ded8a12dabffe127e2
unknown <samueloctavianus97@gmail.com>
2022-05-14 18:11:58 +0700

    finished

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let allBtns = document.querySelector(".button-container");
 let numBtns = document.querySelectorAll(".number");
 let operatorBtns = document.querySelectorAll(".operator");
 let equalsBtn = document.getElementById("equals");
-let delBtn = document.getElementById("delete");
+let delBtn = document.querySelector(".delete");
 let clearBtn = document.getElementById("clear");
 let calcDisplay = document.querySelector(".calculator-display");
 
@@ -32,6 +32,8 @@ allBtns.addEventListener("click", (e) => {
 
   if (target.classList.contains("clear")) {
     console.log("clear", target.value);
+    resetCalculator();
+    updateDisplay();
     return;
   }
 
@@ -41,11 +43,16 @@ allBtns.addEventListener("click", (e) => {
 
 //Input Digit
 function inputDigit(digit) {
-  let { displayValue } = calculator;
-  if (displayValue === "0") {
+  let { displayValue, waitingForSecondOperand } = calculator;
+  if (waitingForSecondOperand === true) {
     calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
   } else {
-    calculator.displayValue = displayValue + digit;
+    if (displayValue === "0") {
+      calculator.displayValue = digit;
+    } else {
+      calculator.displayValue = displayValue + digit;
+    }
   }
   console.log(calculator);
 }
@@ -54,10 +61,41 @@ function inputDigit(digit) {
 function handleOperator(nextOperator) {
   let { firstOperand, displayValue, operator } = calculator;
   let inputValue = Number(displayValue);
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    return;
+  }
   if (firstOperand === null && !isNaN(inputValue)) {
     calculator.firstOperand = inputValue;
+  } else if (operator) {
+    let result = calculate(firstOperand, inputValue, operator);
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
   }
   calculator.waitingForSecondOperand = true;
   calculator.operator = nextOperator;
   console.log(calculator);
+}
+
+//reset calculator
+function resetCalculator() {
+  calculator.displayValue = "0";
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+}
+
+//calculate function
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
 }
